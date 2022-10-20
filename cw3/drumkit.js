bf632b0f901a527b7a83b6e045aa4bf03c0cd5a0
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-10-20 11:25:11 +0200

    update

--- cw3/drumkit.js
+++ cw3/drumkit.js
@@ -8,6 +8,7 @@ function onKeyPress(ev){
     switch(key){
         case '1':
             sound ='tink'
+            ArchiveSound(sound)
             break;
         case '2':
             sound ='hihat'
@@ -18,11 +19,43 @@ function onKeyPress(ev){
         case '4':
             sound ='openhat'
             break;
+        case 'R':
+            PlayChannel1Recording();
+        case 'r':
+            PlayChannel1Recording();
         default:
             sound = 'boom'
             break;
     }
     playSound(sound)
+}
+function ArchiveSound(sound){
+    if(sound == 'tink'){
+        const recording = {
+            sound: sound,
+            time: Date.now()
+        }
+        addChannel1Recording(recording)
+    }
+}
+let channel1Recordings = []
+function addChannel1Recording(channel1recording){
+    channel1Recordings.push(channel1recording)
+}
+
+function PlayChannel1Recording(){
+    for (let i = 0; i < channel1Recordings.length; i++) {
+        playSound(channel1Recordings[i].sound)
+        if(i == channel1Recordings.length-1){
+            break;
+        }
+        let awaitTime = (channel1Recordings[i+1].time - channel1Recordings[i].time)
+        let start = Date.now()
+        do{
+
+        }
+        while(Date.now() - start < awaitTime)
+      }
 }
 
 function playSound(sound){
