217d9be91144b85e90370d3d9f85e3c95f2e6828
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-11-03 12:48:56 +0100

    updated site design and added an option to record and play different channels

--- cw3/drumkit.js
+++ cw3/drumkit.js
@@ -30,6 +30,9 @@ function onKeyPress(ev){
             break;
         case 'r':
             PlayChannelRecording(channel1Recording);
+            PlayChannelRecording(channel2Recording);
+            PlayChannelRecording(channel3Recording);
+            PlayChannelRecording(channel4Recording);
             break;
         default:
             sound = 'boom'
@@ -38,15 +41,44 @@ function onKeyPress(ev){
             break;
     }
 }
+
+const playCh1Buttton = document.getElementById("b_ch1");
+playCh1Buttton.addEventListener("click", () => {PlayChannelRecording(channel1Recording)})
+
+const playCh2Buttton = document.getElementById("b_ch2");
+playCh2Buttton.addEventListener("click", () => {PlayChannelRecording(channel2Recording)})
+
+const playCh3Buttton = document.getElementById("b_ch3");
+playCh3Buttton.addEventListener("click", () => {PlayChannelRecording(channel3Recording)})
+
+const playCh4Buttton = document.getElementById("b_ch4");
+playCh4Buttton.addEventListener("click", () => {PlayChannelRecording(channel4Recording)})
+
 function ArchiveSound(sound){
     let DateNow = new Date()
     const soundRecorded = {
         sound: sound,
         time: DateNow.getTime() //or maybe sth more precise?
     }
-    addSoundToRecording(channel1Recording, soundRecorded)
+    if(document.getElementById('ch1').checked){      
+        addSoundToRecording(channel1Recording, soundRecorded)
+    }
+    if(document.getElementById('ch2').checked){      
+        addSoundToRecording(channel2Recording, soundRecorded)
+    }
+    if(document.getElementById('ch3').checked){      
+        addSoundToRecording(channel3Recording, soundRecorded)
+    }
+    if(document.getElementById('ch4').checked){      
+        addSoundToRecording(channel4Recording, soundRecorded)
+    }
 }
+
 let channel1Recording = []
+let channel2Recording = []
+let channel3Recording = []
+let channel4Recording = []
+
 function addSoundToRecording(channelRecording , newSoundRecorded){
     channelRecording.push(newSoundRecorded)
 }
