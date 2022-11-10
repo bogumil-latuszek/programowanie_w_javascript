d60e6f9b77c87d69710d6f7003efdab75a628948
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-11-10 11:39:43 +0100

    made points 1 2 3

--- cw5/index.js
+++ cw5/index.js
@@ -1,16 +1,40 @@
-interval();
+//let functions_array = [saveCToSessionStorage, saveDToSessionStorage ]
+let functions_array = [(data)=>{saveCToSessionStorage(data); Logger.log(data)}, saveDToSessionStorage() ]
 
-function interval() {
+interval(functions_array);
+StartFuctionsAfterInterval(functions_array, SetRandomInterval);
+
+function interval(functions_array) {
   let timer = 1
   setInterval(
     () => {
       // mamy coupling - interval ma na sztywno zaszyte w sobie C i D (..i logger)
-      saveCToSessionStorage(timer)
-      saveDToSessionStorage(timer)
+      
+      for (let index = 0; index < functions.length; index++) 
+        {
+            functions[index](timer)
+        }
       timer++
     }
     , 2000)
 }
+function StartFuctionsAfterInterval(FunctionsArray, source){
+    source(FunctionsArray)
+}
+function SetRandomInterval(functions) {
+    let timer = Math.random()* 10000
+    setInterval(
+      () => {
+        // mamy coupling - interval ma na sztywno zaszyte w sobie C i D (..i logger)
+        
+        for (let index = 0; index < functions.length; index++) 
+          {
+              functions[index](timer)
+          }
+        timer++
+      }
+      , 2000)
+  }
 
 class Logger {
   static log(data) {
@@ -18,12 +42,21 @@ class Logger {
   }
 }
 
+function saveToSessionStorage(label, log_func = (x)=>{}) {
+    return (data)=>{
+        console.log(`[reader ${label}]`, data)
+        const storageData = { data }
+        sessionStorage.setItem(label, JSON.stringify(storageData))
+        // brudzimy funkcję loggerem - to nie jest jej funkcjonalność!
+        callback(data);
+    }
+  }
+
 function saveCToSessionStorage(data) {
   console.log('[reader C]', data)
   const storageData = { data }
   sessionStorage.setItem('C', JSON.stringify(storageData))
   // brudzimy funkcję loggerem - to nie jest jej funkcjonalność!
-  Logger.log(data)
 }
 
 function saveDToSessionStorage(data) {
