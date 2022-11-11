3c13a0fd02fe59eb27cb43584fe2dd2e72e9d070
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-11-11 11:10:16 +0100

    new functionality: adding and retrieving notes
    notes in very simple form, no way to view notes in html yet, only titles in console

--- cw4/main_script.js
+++ cw4/main_script.js
@@ -1,22 +1,74 @@
+//at start:
+const default_note = {
+    title: "default",
+    content: "default",
+    color: "blue",
+    pinned: true,
+    creationDate: "17.05.2020",
+    tag: "default"
+}
+
+//save default note:
+SaveToLocalStorage( default_note.title, default_note)
+
+//save key to find it later:
+let notes_titles = GetFromLocalStorage("saved_notes_titles")
+if(notes_titles == undefined){
+    notes_titles = [default_note.title]
+}
+else{
+    notes_titles.push(default_note.title)
+}
+SaveToLocalStorage("saved_notes_titles", notes_titles)
+
+function SaveToLocalStorage(key, item){
+    const item_stringified = JSON.stringify(item)
+    localStorage.setItem(key,  item_stringified );
+}
+
+function GetFromLocalStorage(key){
+    const item_stringified = localStorage.getItem(key)
+    const item = JSON.parse(item_stringified)
+    return item
+}
+
 const submit_note = document.getElementById("submit_button");
+
 submit_note.addEventListener("click", ()=>{
-    const default_note = {
-        title: "title",
-        content: "asfafdadsfdsafa",
+
+    const note_container = document.getElementById("note_container")
+    const n_title = note_container.children.title.value
+    const n_content = note_container.children.content.value
+
+    const new_note = {
+        title: n_title,
+        content: n_content,
         color: "blue",
         pinned: true,
         creationDate: "17.05.2020",
         tag: "asfas"
     }
-    const stringified_note = JSON.stringify(default_note)
-    localStorage.setItem("default", stringified_note);
+    
+    SaveToLocalStorage(n_title, new_note)
+
+    let notes_titles = GetFromLocalStorage("saved_notes_titles")
+    let titles_array = notes_titles
+    titles_array.push(n_title)
+    SaveToLocalStorage("saved_notes_titles", titles_array)
 })
 
 const get_note = document.getElementById("get_button");
+
 get_note.addEventListener("click", ()=>{
-    const default_note = localStorage.getItem("default");
-    const title = default_note.title;
+    let notes_titles = GetFromLocalStorage("saved_notes_titles")
+    let titles_array = notes_titles
+    titles_array.forEach(element => {
+        retrieved_note = GetFromLocalStorage(element)
+        title = retrieved_note.title
+        console.log(title)
+    });
 })
+
 const note = {
     title: "title",
     content: "asfafdadsfdsafa",
