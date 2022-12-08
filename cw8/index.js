70bbb5060b625abd1087a0e944338d755caa2f41
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-12-08 11:08:57 +0100

    first working prototype

--- cw8/index.js
+++ cw8/index.js
@@ -1,44 +1,88 @@
-const asyncAdd = async (a,b) => {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-      return Promise.reject('Argumenty muszą mieć typ number!')
-    }
-    return new Promise((resolve, reject) => {
-      setTimeout(() =>{
-        resolve(a+b)
-      }, 100)
-    })
+var start = document.getElementById("start");
+var reset = document.getElementById("reset");
+
+var canv = document.getElementById("canvas");
+var balls = [];
+
+canv.width = canv.offsetWidth;
+canv.height = canv.offsetHeight;
+
+var ctx = canv.getContext("2d");
+ctx.beginPath();
+
+//default data structure
+let ball = {
+    x: 130,
+    y: 60,
+    radius: 5,
+    velocity: 1,
+    last_updated_time: performance.now(),
+    vectorx: 1,
+    vectory: -1
 }
-async function AsyncAddMany (numbers){
-    const num_count = numbers.length;
-    if(num_count == 1 || num_count == 0){
-        return Promise.resolve(numbers)
-    }
-    else{
-        let numbers_halved = [];
-        if(num_count % 2 == 0){
-            for (let i = 0; i < numbers.length; i+=2) {
-                const sumOfTwo = await asyncAdd(numbers[i],numbers[i+1])
-                numbers_halved.push(sumOfTwo);
-            }
+GenerateRandomBalls(30, balls, canv);
+
+animate()
+function animate(){
+    // aktualizuj polozenie 
+    UpdatePositions(balls);
+    //wyczysc canvas
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    // narysuj na canvas
+    DrawBalls(balls);
+    requestAnimationFrame(animate) // this loops to the start, calling itself but will be processed only on request
+}
+
+function GenerateRandomBalls(quantity, balls_list, canvas){
+    for(let i = 0; i <quantity ; i++){
+        const ball = {
+            x: canvas.width * Math.random(),
+            y: canvas.height * Math.random(),
+            radius: 6 +  6 * Math.random(),
+            velocity: 100,
+            last_updated_time: performance.now(),
+            vectorx: -1 + 2 * Math.random(),
+            vectory: -1 + 2 * Math.random()
         }
-        else{
-            numbers_halved.push(numbers.pop())
-            for (let i = 0; i < numbers.length; i+=2) {
-                const sumOfTwo = await asyncAdd(numbers[i],numbers[i+1])
-                numbers_halved.push(sumOfTwo);
-            }
-        }
-        return AsyncAddMany(numbers_halved);
+        balls_list.push(ball);
     }
 }
 
-async function MeasureTime (promise){
-    const start = performance.now();
-    await promise.resolve();
-    const end = performance.now();
-    return Promise.resolve(end - start)
+function UpdatePositions(balls_list){
+    balls_list.forEach(b => {
+        let time_difference = (performance.now() - b.last_updated_time) / 1000 ;// time past last frame(in seconds)
+        b.x = b.x + b.vectorx * b.velocity * time_difference; // calculate new position x 
+        b.y = b.y + b.vectory * b.velocity * time_difference; // calculate new position y
+        b.last_updated_time = performance.now();
+    });
 }
 
-let arr1 = [50,60,65,90];
-let sum =  AsyncAddMany(arr1)
-console.log(sum)
+function DrawBalls(balls_list){
+    balls_list.forEach(element => {
+        ctx.beginPath();
+        ctx.arc(element.x, element.y, element.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    });
+}
+
+/*
+animate()
+function animate(){
+    // aktualizuj polozenie 
+    // narysuj na canvas
+    requestAnimationFrame(animate) // this loops to the start, calling itself but will be processed only on request
+}
+
+async func actualPoloz(kulka){
+    return kulka *2
+}
+
+const kulki =[1,2,3,45,6]
+
+const nowe polozenia promise = [];
+for (const kulka of kulki) {
+    nowePolozeniaPromise.push(aktualizujPoloz(kulka));
+}
+
+const result = await Promise.all(nowePolozeniaPromise);
+*/
--- cw8/readme.md
+++ cw8/readme.md
@@ -1,7 +1,13 @@
-Zadanie
-Napisz funkcję która pobiera dowolną ilość argumentów (liczby całkowite) i następnie korzysta z asyncAdd() by je dodać.
-Napisz funkcję mierzącą czas wykonania kodu.
-Zmierz działanie funkcji dodającej dla zbiorów danych o wielkości 100 elementów. Wyświetl czas wykonania oraz ilość operacji asynchronicznych.
-Zoptymalizuj działanie swojej funkcji pod kątem czasu wykonania
-Przydamisię
-Mierzenie czasu wykonywania skryptu: performance.now() lub performance.mark(name) + performance.measure(name, startMark, endMa
+Wersja Nic się nie działo, naprawdę nic się... :
+Narysuj X kulek poruszających się w dowolnym kierunku z losową prędkością
+Jeśli odległość pomiędzy kulkami jest mniejsza niż Y rysuj pomiędzy nimi linię
+Kulki odbijają się od krawędzi strony
+Dodaj przyciski Start i Reset
+Zbadaj ile jesteś w stanie wyświetlić kulek (stabilne 60fps) przy założeniu Y = 20% szerokości ekranu
+X, Y jest definiowane przez użytkownika (pola tekstowe lub np. suwaki - wstępnie uzupełnione)
+
+Przydamisie:
+Rysowanie koła: ctx.arc().
+Rysownie linii: ctx.beginPath(), ctx.moveTo(), ctx.lineTo()
+Wypełnianie/obrysowanie kształtu: ctx.fill(), ctx.stroke()
+Czyszczenie canvas: ctx.clearRect()
