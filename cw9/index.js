878b4e9be0c37ce8e1395ac3df58e32d889e9362
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-01-05 00:25:03 +0100

    added functionality to delete button

--- cw9/index.js
+++ cw9/index.js
@@ -1,7 +1,7 @@
 let canvasas = document.getElementById("canvasas");
 let weather_list =document.getElementById("weather_info_list"); 
-let submit_city_button = document.getElementById("submit_city")
-let city_name_textbox = document.getElementById("input_city_name")
+let submit_city_button = document.getElementById("submit_city");
+let city_name_textbox = document.getElementById("input_city_name");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -34,7 +34,7 @@ submit_city_button.addEventListener("click", ()=>{
         const city_name = city_name_textbox.value
         getWeather(city_name).then((weather)=>
         {
-            DisplayCityWeather(weather)
+            DisplayCityWeather(city_name, weather)
         })
     }
     catch( e ){
@@ -72,23 +72,47 @@ let current_wether = {
     humidityPercent: 40,
     cloudiness: 0
 }
-
-function DisplayCityWeather(weather){
-    const HtmlWeatherContainer = CreateHtmlWeatherContainer(weather)
-    weather_list.appendChild(HtmlWeatherContainer);
+function removeItemFromMemory(){
+    DeleteCityName(this.attributes.city_name.value)
+    this.parentNode.remove();
 }
-function CreateHtmlWeatherContainer(weather){
+
+function DisplayCityWeather(city_name, weather){
+    const HtmlCityWeatherContainer = CreateHtmlCityWeatherContainer(city_name, weather)
+    weather_list.appendChild(HtmlCityWeatherContainer);
+}
+function CreateHtmlCityWeatherContainer(city_name, weather){
     var main_container = document.createElement("div");//create cont to hold weather description and icon
     main_container.setAttribute("class","main_cont");
-    var description_container = document.createElement("div");
-    description_container.setAttribute("class","descr_cont");
-    description_container.innerText = 'weather in '+ weather.name +' is ' + weather.weather[0].description
-    main_container.appendChild(description_container);//add description to main cont
+    var city_name_container = document.createElement("div");
+    city_name_container.innerText = weather.name;
+    var temperature_container = document.createElement("div");
+    temperature_container.innerText = Math.ceil(weather.main.temp -  273.15) +"°C"
+    var humidity_container = document.createElement("div");
+    humidity_container.innerText = weather.main.humidity +"% Humidity"
+    //var description_container = document.createElement("div");
+    city_name_container.setAttribute("class","descr_cont");
+    humidity_container.setAttribute("class","descr_cont");
+    temperature_container.setAttribute("class","descr_cont");
+    //main_container.setAttribute("class","descr_cont");
+    main_container.appendChild(city_name_container);
+    main_container.appendChild(temperature_container);
+    main_container.appendChild(humidity_container);
+    //description_container.innerText = 'weather in '+ weather.name +' is ' + weather.weather[0].description
+    //main_container.appendChild(description_container);//add description to main cont
     var icon_container = document.createElement("img") 
     icon_container.setAttribute("class","icon_cont");
     let icon_code = weather.weather[0].icon; 
     icon_container.src = 'http://openweathermap.org/img/wn/'+ icon_code +'@2x.png';
     main_container.appendChild(icon_container); // add icon to main cont
+
+    var delete_button = document.createElement("button");
+    delete_button.setAttribute("class","delete_button");
+    delete_button.setAttribute("city_name", city_name);
+    delete_button.innerText = "X";
+    delete_button.addEventListener("click", removeItemFromMemory);
+
+    main_container.appendChild(delete_button);
     return main_container;
 }
 function SaveToLocalStorage(key, item){
@@ -102,6 +126,9 @@ function GetFromLocalStorage(key){
     return item
 }
 
+function DeleteFromLocalStorage(key){
+    localStorage.removeItem(key);
+}
 
 
 async function SaveCityName(city_name){
@@ -114,7 +141,20 @@ async function SaveCityName(city_name){
         SaveToLocalStorage("saved_cities", saved_cities);
     }
 }
-
+async function DeleteCityName(name_to_delete){
+        let saved_cities = GetFromLocalStorage("saved_cities");
+        if(saved_cities == null){
+            return 0;
+        }
+        let new_saved_cities = [];
+        for (let i = 0; i < saved_cities.length; i++) {
+            const name = saved_cities[i];
+            if (name != name_to_delete) {
+                new_saved_cities.push(name);
+            }
+        }
+        SaveToLocalStorage("saved_cities", new_saved_cities);
+}
 function CityNameAlreadySaved(city_name){
     let saved_cities = [];
     try{
@@ -160,12 +200,11 @@ saved_cities.forEach(city_name => {
     try{
         getWeather(city_name).then((weather)=>
         {
-            DisplayCityWeather(weather)
+            DisplayCityWeather(city_name, weather)
         })
     }
     catch( e ){
         console.log("weather not found for this city name")
     }
 });
-
 //localStorage.getItem(key) localStorage.setItem(key, stringValue) localStorage.removeItem(key) localStorage.clear()
