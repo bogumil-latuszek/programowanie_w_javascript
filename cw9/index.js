dec34e86ae75a9c1380a77f26969a2c18e5d8437
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-01-12 07:19:19 +0100

    added saving weather data to browser memory
    +small refactor of existing functions

--- cw9/index.js
+++ cw9/index.js
@@ -36,7 +36,10 @@ submit_city_button.addEventListener("click", ()=>{
             const city_name = city_name_textbox.value
             getWeather(city_name).then((weather)=>
             {
-                DisplayCityWeather(city_name, weather)
+                if (weather != null) {
+                    SaveInfoCopy(city_name, weather.main.temp, weather.main.humidity, weather.weather[0].icon) 
+                    DisplayCityWeather(city_name, weather.main.temp, weather.main.humidity, weather.weather[0].icon)
+                }
             })
         }
     }
@@ -48,20 +51,27 @@ submit_city_button.addEventListener("click", ()=>{
 async function getWeather(city_name)
 {
     // api.getweather(city_name)
-    const full_adress = 'https://api.openweathermap.org/data/2.5/weather?q='+ city_name +'&APPID=d5976b89430eb34f49cf05f0740ad27e'
-    let resp = await fetch(full_adress);
-    if(resp.ok)
-    {
-        SaveCityName(city_name);// remember the location
-        console.log("ok");
-        return new Promise((resolve, reject) => 
+    try {
+        const full_adress = 'https://api.openweathermap.org/data/2.5/weather?q='+ city_name +'&APPID=d5976b89430eb34f49cf05f0740ad27e'
+        let resp = await fetch(full_adress);
+        if(resp.ok)
         {
-            resolve(resp.json());
-        })
-    }
-    else{
-        console.log("error");
-        throw new Error("not found");
+            SaveCityName(city_name);// remember the location
+            console.log("ok");
+            return new Promise((resolve, reject) => 
+            {
+                resolve(resp.json());
+            })
+        }
+        else{
+            console.log("error");
+            throw new Error("not found");
+        }
+    } catch (error) {
+        return new Promise((resolve, reject) => 
+            {
+                resolve(null);
+            })
     }
 }
 
@@ -73,17 +83,16 @@ let current_wether = {
     cloudiness: 0
 }
 function removeItemFromMemory(){
+    DeleteCityInfo(this.attributes.city_name.value)
     DeleteCityName(this.attributes.city_name.value)
     this.parentNode.remove();
 }
 
-function DisplayCityWeather(city_name, weather){
-    const HtmlCityWeatherContainer = CreateHtmlCityWeather(city_name, weather)
+function DisplayCityWeather(city_name, temperature, humidity, icon){
+    const HtmlCityWeatherContainer = CreateHtmlCityWeatherContainer(city_name, temperature, humidity, icon)
     weather_list.appendChild(HtmlCityWeatherContainer);
 }
-function CreateHtmlCityWeather(city_name, weather){
-    return CreateHtmlCityWeatherContainer(city_name, weather.main.temp, weather.main.humidity, weather.weather[0].icon)
-}
+
 function CreateHtmlCityWeatherContainer(city_name, temperature, humidity, icon){
     var main_container = document.createElement("div");//create cont to hold weather description and icon
     main_container.setAttribute("class","main_cont");
@@ -198,13 +207,25 @@ function GetAllCityNames(){
 
 function refresh(){
     weather_list.innerHTML ='';
-    let saved_cities = GetAllCityNames();
+    let saved_cities_info = GetAllCitiesInfo()
+    saved_cities_info.forEach(city_info => {
+        try{
+            DisplayCityWeather(city_info.city_name, city_info.temperature, city_info.humidity, city_info.icon)
+        }
+        catch( e ){
+            console.log("weather not found for this city name")
+        }
+    })
+
+    let saved_cities_names = GetAllCityNames();
+
+    weather_list.innerHTML ='';
     //on start: load every city info
-    saved_cities.forEach(city_name => {
+    saved_cities_names.forEach(city_name => {
     try{
         getWeather(city_name).then((weather)=>
         {
-            DisplayCityWeather(city_name, weather)
+            DisplayCityWeather(city_name, weather.main.temp, weather.main.humidity, weather.weather[0].icon)
         })
     }
     catch( e ){
@@ -218,3 +239,76 @@ function refresh(){
 refresh();
 setInterval(refresh, 5*60000);
 //localStorage.getItem(key) localStorage.setItem(key, stringValue) localStorage.removeItem(key) localStorage.clear()
+
+
+async function SaveInfoCopy(city_name, temperature, humidity, icon){
+    let city_weather_container = {
+        city_name : city_name,
+        temperature: temperature,
+        humidity : humidity,
+        icon : icon
+    }
+    SaveCityInfo(city_weather_container)
+}
+
+async function SaveCityInfo(city_info){
+    if(!CityInfoAlreadySaved(city_info)){
+        let saved_cities_info = GetFromLocalStorage("saved_cities_info");
+        if(saved_cities_info == null){
+            saved_cities_info = [];
+        }
+        if (saved_cities_info.length <10) {
+            saved_cities_info.push(city_info);
+            SaveToLocalStorage("saved_cities_info", saved_cities_info);
+        }    
+    }
+}
+async function DeleteCityInfo(city_to_delete){
+        let saved_cities_info = GetFromLocalStorage("saved_cities_info");
+        if(saved_cities_info == null){
+            return 0;
+        }
+        let new_saved_cities_info = [];
+        for (let i = 0; i < saved_cities_info.length; i++) {
+            const info = saved_cities_info[i];
+            if (info.city_name != city_to_delete) {
+                new_saved_cities_info.push(info);
+            }
+        }
+        SaveToLocalStorage("saved_cities_info", new_saved_cities_info);
+}
+function CityInfoAlreadySaved(city_info){
+    let saved_cities_info = [];
+    try{
+        saved_cities_info = GetFromLocalStorage("saved_cities_info");
+        if(saved_cities_info == null){
+            return false;
+        }
+    }
+    catch(e){
+        return false;
+    }
+    let found = false;
+    saved_cities_info.forEach(info => {
+        if(info.city_name==city_info.city_name){
+            found = true;
+        }
+    });
+    return found;
+}
+
+function GetAllCitiesInfo(){
+    let saved_cities_info = [];
+    try{
+        saved_cities_info = GetFromLocalStorage("saved_cities_info");
+    }
+    catch(e){
+        console.log(e);
+    }
+    finally{
+        if(saved_cities_info == null){
+            saved_cities_info = [];
+        }
+        return saved_cities_info;
+    }
+}
