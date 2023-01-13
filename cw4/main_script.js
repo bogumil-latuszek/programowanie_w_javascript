c21fb5fe3e96f18cb30a455b3e19f906758ed171
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-01-13 05:14:14 +0100

    Site UI update + new functionality for deleting notes

--- cw4/main_script.js
+++ cw4/main_script.js
@@ -8,7 +8,8 @@ const default_note = {
     tag: "default"
 }
 
-//save default note:
+//save default note: 
+/*
 let saved_default_note = GetFromLocalStorage(default_note.title)
 if(saved_default_note == undefined){
     SaveToLocalStorage( default_note.title, default_note)
@@ -22,7 +23,7 @@ if(saved_default_note == undefined){
     }
     SaveToLocalStorage("saved_notes_titles", notes_titles)
 }
-
+*/
 
 function SaveToLocalStorage(key, item){
     const item_stringified = JSON.stringify(item)
@@ -102,5 +103,38 @@ function CleanRenderSpace(){
     saved_notes.innerHTML = ""
 }
 function RenderNote(object){
-    saved_notes.innerHTML += `<div class="note" style="background-color: ${object.color};" > <h>${object.title}</h> <br> ${object.content} <br> ${object.creationDate}</div>`
+    var saved_note = document.createElement("div")
+    saved_note.className = "saved_note"
+    saved_note.innerHTML += `<div class="note" style="background-color: ${object.color};" > <h>${object.title}</h> <br> ${object.content} <br> ${object.creationDate}</div>`
+
+    var delete_button = document.createElement("button");
+    delete_button.setAttribute("class","delete_button");
+    delete_button.setAttribute("title", object.title);
+    delete_button.innerText = "X";
+    delete_button.addEventListener("click", removeItemFromMemory);
+    
+    saved_note.appendChild(delete_button);
+
+    saved_notes.appendChild(saved_note)
+}
+
+function removeItemFromMemory(){
+    DeleteNote(this.attributes.title.value)
+    this.parentNode.remove();
+}
+
+async function DeleteNote(note_title){
+    let saved_titles = GetFromLocalStorage("saved_notes_titles");
+    if(saved_titles == null){
+        return 0;
+    }
+    let new_saved_titles = [];
+    for (let i = 0; i < saved_titles.length; i++) {
+        const title = saved_titles[i];
+        if (title != note_title) {
+            new_saved_titles.push(title);
+        }
+    }
+    SaveToLocalStorage("saved_notes_titles", new_saved_titles);
+    localStorage.removeItem(note_title);
 }
