c673d16515415615000777d2149fab1ce82cd916
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-01-15 19:38:32 +0100

    added auto scrolling and a pause button

--- cw2/main_script.js
+++ cw2/main_script.js
@@ -1,23 +1,42 @@
-// notatnik z zajęć
-const timeoutRef = setTimeout( 
-    () => {
-        main.innerHTML='From setTimeout'
-    },
-    2000
-)
-let licznik = 0 
-const intervalRef = setInterval( 
-    () => {
-        main.innerHTML='From interval' + licznik++
-    },
-    4000
-)
+var glider = new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+})
 
-// kasujemy setInterval
-clearInterval(intervalRef)
+let pause_button = document.getElementById("stop")
+pause_button.addEventListener("click", ()=>{
+    if (cont == true) {
+        cont = false;
+        document.getElementById("stop_img").src="./images/play.png"
+    }
+    else{
+        cont = true;
+        document.getElementById("stop_img").src="./images/pause.png"
+        StartAnimation();
+    }
+})
 
-// kasujemy setTimeout
-clearTimeout(intervalRef)
+let curr_index = 0
+let cont = true
+async function StartAnimation(){
+    setTimeout(() => {
+        if (cont == false) {
+            return;
+        }
+        if (curr_index < 2) {
+            curr_index++
+        }
+        else{
+            curr_index = 0;
+        }
+        glider.scrollItem(curr_index, false)
+        requestAnimationFrame(StartAnimation)
+    }, 2000)
+}
 
-
-// window.requestAnimationFrame
+StartAnimation()
